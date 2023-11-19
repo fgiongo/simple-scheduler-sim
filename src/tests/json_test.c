4b7c32177a3620ceb06a2ef8906fe2ac4bba49f4
Hackzzin <matheushackbus@gmail.com>
2023-11-19 19:49:02 -0300

    c

--- src/tests/json_test.c
+++ src/tests/json_test.c
@@ -5,7 +5,10 @@ int main (void){
     DynamicArray *arrayD;
     char *toString;
 
+    ProcessQueue* queue;
     arrayD = initialize_array();
+    queue = parse_JSON(arrayD);
+
     for(i = 0; i < arrayD -> size; i++){
         toString = process_tostring(arrayD -> Darray[i], 1024);
         printf("%s", toString);
